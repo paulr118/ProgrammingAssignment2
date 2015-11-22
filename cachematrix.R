94b43bcfbadec4b1144499f136bb2d0e89757a02
Paul Reeves <paulr118@hotmail.com>
2015-11-22 12:45:22 +0000

    PR_CacheMatrix
    This R project contains two functions, 'makeCacheMatrix' which create a
    special matrix object and 'cachesSolve' which uses the object to
    determine if there is a cached result.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions compute the inverse of a matrix and cache the result
 
-## Write a short comment describing this function
+## Creates a special matrix object to pass to cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
+
+
+	#set matrix initialised to null
+	matrix <- NULL
+
+	# matrix x set to new object y with the inverse matrix reset to null
+	set <- function(y) {
+		x <<- y
+		matrix <<- NULL
+	}
+
+	#get matrix
+	get <- function() x
+
+	#calculates inverse matrix using solve function
+	setinvmatrix<- function(solve) matrix <<-solve
+
+	#return inverse matrix 
+	getinvmatrix <- function() matrix
+	
+	#makeCacheMatrix function output
+	list(set = set, get = get,
+	setinvmatrix = setinvmatrix,
+	getinvmatrix = getinvmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## The cachesolve function will try and retrieve the matrix inverse from the cache.
+##if it fails it will calculate the inverse of the input matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+       #get cached inverse matrix 
+	matrix <- x$getinvmatrix()
+
+	#return inverse matrix from cache 
+	if(!is.null(matrix)) {
+		message("getting cached data")
+		return(matrix)
+	}
+
+	#if no cache record exists, retrieve data to calculate inverse matrix 
+	data <- x$get() 
+	matrix <- solve(data, ...)
+	x$setinvmatrix(matrix)
+	
+	#display inverse matrix output 
+	matrix
 }
